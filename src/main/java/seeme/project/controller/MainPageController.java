447fcdc0847d86bed588b8be8f76499d1cd4e298
kucnea <kucnea@naver.com>
2022-05-19 22:48:44 +0900

    test git ignore

--- src/main/java/seeme/project/controller/MainPageController.java
+++ src/main/java/seeme/project/controller/MainPageController.java
@@ -4,53 +4,58 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import seeme.project.domain.Viewer;
 
-@Controller // View를 리턴하겠다
+//@Controller // View를 리턴하겠다
+@RestController // json 형식을 이용하기위한 컨트롤러 어노테이션
 @Slf4j
 @RequestMapping("/")
 public class MainPageController {
 
-    @GetMapping({"","/"})
-    public String index(Model model){
-        model.addAttribute("data","hello");
-        log.info("INDEX Page OnLoad");
-        return "hello";
+
+    @GetMapping("/")
+    public Object index(){
+        Viewer viewer = new Viewer("id","pw",1);
+        return viewer;
     }
-
-    @GetMapping("next-page")
-    public String nextPage(@RequestParam("name") String name, Model model) {
-
-        model.addAttribute("name", name);
-        return "next-page";
-
-    }
-
-
-    //JSON Test
-    @GetMapping("helloapi")
-    @ResponseBody
-    public Hello helloApi(@RequestParam("name") @Nullable String name){
-            Hello hello = new Hello();
-            hello.setName(name);
-
-        return hello; // JSON 방식으로 페이지에 데이터가 전송됨
-    }
-
-
-
-    static class Hello{
-        private String name;
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-    }
+//    @GetMapping({"","/"})
+//    public String index(Model model){
+//        model.addAttribute("data","hello");
+//        log.info("INDEX Page OnLoad");
+//        return "hello";
+//    }
+//
+//    @GetMapping("next-page")
+//    public String nextPage(@RequestParam("name") String name, Model model) {
+//
+//        model.addAttribute("name", name);
+//        return "next-page";
+//
+//    }
+//
+//
+//    //JSON Test
+//    @GetMapping("helloapi")
+//    @ResponseBody
+//    public Hello helloApi(@RequestParam("name") @Nullable String name){
+//            Hello hello = new Hello();
+//            hello.setName(name);
+//
+//        return hello; // JSON 방식으로 페이지에 데이터가 전송됨
+//    }
+//
+//
+//
+//    static class Hello{
+//        private String name;
+//
+//        public String getName() {
+//            return name;
+//        }
+//
+//        public void setName(String name) {
+//            this.name = name;
+//        }
+//    }
 }
