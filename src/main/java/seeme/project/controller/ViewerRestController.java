612bb9764ee4761123785b9009127b8fbb541232
kucnea <kucnea@naver.com>
2022-05-22 17:33:31 +0900

    loginPage creating

--- src/main/java/seeme/project/controller/ViewerRestController.java
+++ src/main/java/seeme/project/controller/ViewerRestController.java
@@ -1,19 +1,42 @@
 package seeme.project.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import seeme.project.domain.Viewer;
+import seeme.project.service.ViewerService;
 
 @RestController // Json 형식을 리턴하는 Controller
 @Slf4j
 @RequestMapping("/viewer")
 public class ViewerRestController {
 
+    // 생성자 주입
+    private final ViewerService viewerService;
+
+    // 스프링 컨테이너에 등록하면 하나만 넣어 쓸 수 있음, 다른 이점도 있음
+    @Autowired
+    public ViewerRestController(ViewerService viewerService){ // 이대로만 쓰면, service를 spring이 알 수 없음.
+        this.viewerService = viewerService;               // ViewerService에 @Service 추가
+    }
+
     @PostMapping("/viewerjoin.do")
     public Object viewerJoin(@RequestBody Viewer viewer) {
         log.info("● ● ● Into viewerJoin.do");
         log.info(viewer.getVId()+" : "+viewer.getVPw());
+
+        viewer = viewerService.join(viewer);
+
         return viewer;
     }
 
+    @GetMapping("/viewerlogin.do")
+    public Object viewerLogin(@RequestBody Viewer viewer){
+        log.info("● ● ● Into viewerLogin.do");
+        log.info(viewer.getVId()+" : "+viewer.getVPw());
+
+        viewer = viewerService.findOneByVId(viewer.getVId()).get();
+
+        return viewer;
+    }
 }
